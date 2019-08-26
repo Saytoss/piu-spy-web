3720a7da2f5d1285db9577db4b62647525eac7bd
Anton Kalmanovich <grumd0@gmail.com>
2019-08-26 22:38:10 +0300

    Updates

---
+++ precache-manifest.1f8680e62720a5439182abba4a871d17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c296175ead206711acf70d56b966a521",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b4491e541c9286332c3",
+    "url": "/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "808d50e5509445927973",
+    "url": "/static/css/main.11e17609.chunk.css"
+  },
+  {
+    "revision": "4b4491e541c9286332c3",
+    "url": "/static/js/2.f37ee4fc.chunk.js"
+  },
+  {
+    "revision": "808d50e5509445927973",
+    "url": "/static/js/main.5ad33fef.chunk.js"
+  },
+  {
+    "revision": "d866d73215f6f8c03d44",
+    "url": "/static/js/runtime~main.c65bc6cb.js"
+  }
+]);
