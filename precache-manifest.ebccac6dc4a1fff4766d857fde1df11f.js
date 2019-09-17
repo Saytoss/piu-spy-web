37c90e38df005c1bf6c0b67c4d7c20f9faa9f69a
Anton Kalmanovich <grumd0@gmail.com>
2019-09-17 12:40:03 +0300

    Updates

---
+++ precache-manifest.ebccac6dc4a1fff4766d857fde1df11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a49d850dce565a328452c64c8a20c3c",
+    "url": "/piu-spy-web/index.html"
+  },
+  {
+    "revision": "5dff1edb28f5a124f770",
+    "url": "/piu-spy-web/static/css/2.1bda2a1b.chunk.css"
+  },
+  {
+    "revision": "8d9d25f2746ddd499117",
+    "url": "/piu-spy-web/static/css/main.f1871ae2.chunk.css"
+  },
+  {
+    "revision": "5dff1edb28f5a124f770",
+    "url": "/piu-spy-web/static/js/2.2eda3803.chunk.js"
+  },
+  {
+    "revision": "8d9d25f2746ddd499117",
+    "url": "/piu-spy-web/static/js/main.129331bf.chunk.js"
+  },
+  {
+    "revision": "15a1a10d8a46dd456db9",
+    "url": "/piu-spy-web/static/js/runtime~main.43c989ce.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/piu-spy-web/precache-manifest.e7afd7f2e46d7c34593641b9c50d796d.js"
+  "/piu-spy-web/precache-manifest.ebccac6dc4a1fff4766d857fde1df11f.js"
 );
 
 self.addEventListener('message', (event) => {
