f27d935761b01f46f58ad65e2fd9648612976053
Anton Kalmanovich <grumd0@gmail.com>
2019-10-14 19:57:17 +0300

    Updates

---
+++ precache-manifest.4bfd19ac52292d3ae38f3921e8412aba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89afdeb5a70ae8373d9ebc4329c3b9a2",
+    "url": "/piu-spy-web/index.html"
+  },
+  {
+    "revision": "68474203be07f3580cce",
+    "url": "/piu-spy-web/static/css/2.1bda2a1b.chunk.css"
+  },
+  {
+    "revision": "9d61ee86cd6a7b13ac48",
+    "url": "/piu-spy-web/static/css/main.0aecaf2a.chunk.css"
+  },
+  {
+    "revision": "68474203be07f3580cce",
+    "url": "/piu-spy-web/static/js/2.b4168106.chunk.js"
+  },
+  {
+    "revision": "9d61ee86cd6a7b13ac48",
+    "url": "/piu-spy-web/static/js/main.532f3fc3.chunk.js"
+  },
+  {
+    "revision": "15a1a10d8a46dd456db9",
+    "url": "/piu-spy-web/static/js/runtime~main.43c989ce.js"
+  }
+]);
