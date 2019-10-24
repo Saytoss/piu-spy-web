65cab50b7350f14e0d5693d31970a096f96b606b
Anton Kalmanovich <grumd0@gmail.com>
2019-10-24 16:49:35 +0300

    Updates

--- precache-manifest.72efb22991e13bd7cbea4948d58ea858.js
+++ precache-manifest.72efb22991e13bd7cbea4948d58ea858.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aff34d039a34c9f2d10bfa01933b15dd",
+    "revision": "81d84e6aa8519910cc2cf313ec4b6d80",
     "url": "/piu-spy-web/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/piu-spy-web/static/css/2.1bda2a1b.chunk.css"
   },
   {
-    "revision": "8dbeea1a09dbc3ddf1a7",
-    "url": "/piu-spy-web/static/css/main.032aed96.chunk.css"
+    "revision": "80c209006ee3990a87f5",
+    "url": "/piu-spy-web/static/css/main.e83ccc79.chunk.css"
   },
   {
     "revision": "27da684b15f86fb17556",
     "url": "/piu-spy-web/static/js/2.775dbd66.chunk.js"
   },
   {
-    "revision": "8dbeea1a09dbc3ddf1a7",
-    "url": "/piu-spy-web/static/js/main.ba8854bf.chunk.js"
+    "revision": "80c209006ee3990a87f5",
+    "url": "/piu-spy-web/static/js/main.16224183.chunk.js"
   },
   {
     "revision": "15a1a10d8a46dd456db9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/piu-spy-web/precache-manifest.5d30cd8631f3574111025f3c866f3c6d.js"
+  "/piu-spy-web/precache-manifest.72efb22991e13bd7cbea4948d58ea858.js"
 );
 
 self.addEventListener('message', (event) => {
