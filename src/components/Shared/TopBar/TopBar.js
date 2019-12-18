5a258afa72316b3e6bf86bfa3f0527865499d2a3
Anton Kalmanovich <grumd0@gmail.com>
2019-12-18 18:31:13 +0200

    Added button

--- src/components/Shared/TopBar/TopBar.js
+++ src/components/Shared/TopBar/TopBar.js
@@ -1,35 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 // import toBe from 'prop-types';
 // import classNames from 'classnames';
 import { NavLink } from 'react-router-dom';
+import GoogleLogin from 'react-google-login';
+import { FaGoogle } from 'react-icons/fa';
 
 import './top-bar.scss';
 
 import { routes } from 'constants/routes';
 
-class TopBar extends Component {
-  static propTypes = {};
-
-  render() {
-    return (
-      <header className="top-bar">
-        <nav>
-          <ul>
-            <li>
-              <NavLink exact to={routes.leaderboard.path}>
-                leaderboard
-              </NavLink>
-            </li>
-            <li>
-              <NavLink exact to={routes.ranking.path}>
-                ranking
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
+function TopBar() {
+  const onGoogleResponse = res => {
+    console.log(res);
+  };
+  return (
+    <header className="top-bar">
+      <nav>
+        <ul>
+          <li>
+            <NavLink exact to={routes.leaderboard.path}>
+              leaderboard
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to={routes.ranking.path}>
+              ranking
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className="_flex-fill" />
+      <div className="login-container">
+        <GoogleLogin
+          clientId="197132042723-cmibep21qf6dald9l2l01rif7l5dtd4s.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={onGoogleResponse}
+          onFailure={onGoogleResponse}
+          cookiePolicy={'single_host_origin'}
+          render={({ onClick, disabled }) => (
+            <button className="btn btn-dark btn-icon btn-sm" onClick={onClick} disabled={disabled}>
+              <FaGoogle />
+              <span> login</span>
+            </button>
+          )}
+        />
+      </div>
+    </header>
+  );
 }
-
 export default TopBar;
